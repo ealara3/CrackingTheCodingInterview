93c6f47375f69913cc66856d18dea836fd1b372c
ealara3 <ealara3@miners.utep.edu>
2018-04-16 14:36:14 -0600

    Chapter3 3.3

--- Problem 3.3/Problem3.java
+++ Problem 3.3/Problem3.java
@@ -1,10 +1,10 @@
-import java.util.LinkedList;
+import java.util.Stack;
 
 public class Problem3 {
-	static int index = -1;
-	static LinkedList<Integer> ls = new LinkedList<Integer>();
+	//static int index = -1;
+	//static LinkedList<Integer> ls = new LinkedList<Integer>();
 	
-public static void push(int n){
+/*public static void push(int n){
 	ls.add(n);
 	index++;
 }
@@ -19,15 +19,50 @@ public static int popAt(int n){
 	int remove = ls.get(n);
 	ls.remove(n);
 	return remove;
-}
+}*/
+	static Stack<Integer> st= new Stack<Integer>();
+	static Stack<Integer> st2 = new Stack<Integer>();
+	static Stack<Integer> st3 = new Stack<Integer>();
+	static Stack<Integer> st4 = new Stack<Integer>();
+	
+	public static int popAt(int n){
+		int i=0;
+		switch(n){
+		case 1:
+			i = (int) st.pop();
+			break;
+		case 2:
+			i = (int) st2.pop();
+			break;
+		case 3:
+			i = (int) st3.pop();
+			break;
+		case 4:
+			i = (int) st4.pop();
+			break;
+		}
+	return i;	
+	}
 	
 public static void main(String arg[]){
-	push(5);
-	push(19);
-	push(500);
-	push(42);
-	push(18);
+	st.push(5);
+	st.push(19);
+	st.push(500);
+	st.push(42);
+	st.push(18);
+	st2.push(15);
+	st2.push(25);
+	st2.push(85);
+	st2.push(15);
+	st3.push(79);
+	st3.push(98);
+	st3.push(75);
+	st3.push(54);
+	st4.push(2);
+	st4.push(198);
+	st4.push(354);
+	st4.push(189);
 	
-	System.out.println(popAt(3));
+	System.out.println(popAt(4));
 }
 }
--- Problem 3.3/README.md.txt
+++ Problem 3.3/README.md.txt
@@ -1,11 +1,9 @@
 This problem is in the book Cracking the problem interview Chapter 3 problem 3.3 page 99.
 
-The begining of this problems is telling something that I didn't understand and I didn't know if we had to implement it in this lab either.
+What the problem is asking is to, (once one stack is full and creat another stack) it need to pop a certain stack.
 
-All what I understood about this problem is to include a method to the stack that can pop at what ever index.
+The problem is asking to reach a certain stack.
 
-The goal is to pop at what ever index is given.
-
-The way I am planing to solve this, is by using the code I did in problem one and use that linked list implementation and just remove it from the stack.
+How I plan to reach a problem is by making a switch statement, that if it is planned to pop the fourth stack, then it goes and pop that certain stack.
 
 This will work for what ever element is implemented.
